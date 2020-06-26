3b5c792625836cc2e5b6f66031ce4eba8441da7e
genekomarov <gene.komarov@gmail.com>
2020-06-26 13:49:20 +0500

    Updates

---
+++ precache-manifest.d3d226ef0fd3e2a6fa40ad2b3ed521ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c26f8eb314e6af0b38b0ae7866f98f0",
+    "url": "/task-manager_mui/index.html"
+  },
+  {
+    "revision": "2ae5e97f352c6c70cf1f",
+    "url": "/task-manager_mui/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "a61fc817241cae6dd0be",
+    "url": "/task-manager_mui/static/js/2.88a42ec8.chunk.js"
+  },
+  {
+    "revision": "b5ed9a26d080d02453cd1ad8610813a6",
+    "url": "/task-manager_mui/static/js/2.88a42ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae5e97f352c6c70cf1f",
+    "url": "/task-manager_mui/static/js/main.cd9f6292.chunk.js"
+  },
+  {
+    "revision": "b72e224f1c0e4d110afa",
+    "url": "/task-manager_mui/static/js/runtime-main.4c89a7af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/task-manager_mui/precache-manifest.64b17fb7851c4e853bc46731cbbbd543.js"
+  "/task-manager_mui/precache-manifest.d3d226ef0fd3e2a6fa40ad2b3ed521ce.js"
 );
 
 self.addEventListener('message', (event) => {
